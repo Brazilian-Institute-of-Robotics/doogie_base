75c36020bea1a6bb1d479890a04539d47f103cd2
Mateus Menezes <mateusmenezes95@gmail.com>
2019-11-22 17:52:27 -0300

    Add MoveBase class

---
+++ doogie_navigation/src/move_base_node.cpp
@@ -0,0 +1,13 @@
+#include <ros/ros.h>
+#include <doogie_navigation/move_base.hpp>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "move_base_node");
+
+  ros::NodeHandle robot_base_nh("move_base_controller");
+  doogie_navigation::MoveBase move_base(robot_base_nh);
+
+  move_base.start();
+
+  return 0;
+}
