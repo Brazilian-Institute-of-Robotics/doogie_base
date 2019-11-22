6ff45a034d23ea9d685d4c3cd44b389e1fe6f0c8
Caio <caioaamaral@gmail.com>
2019-11-22 16:25:52 -0300

    Add GlobalToLocal MatrixHandle method

--- doogie_navigation/include/doogie_navigation/matrix_handle.hpp
+++ doogie_navigation/include/doogie_navigation/matrix_handle.hpp
@@ -4,10 +4,17 @@
 #include <ros/ros.h>
 #include "doogie_msgs/MazeCell.h"
 #include "doogie_msgs/MazeCellMultiArray.h"
+#include "doogie_msgs/DoogiePosition.h"
 
 #include <string>
 
 namespace doogie_navigation {
+typedef struct LocalCell{
+  bool front_wall;
+  bool back_wall;
+  bool right_wall;
+  bool left_wall;
+}LocalCell;
 
 class MatrixHandle {
  public:
@@ -16,6 +23,7 @@ class MatrixHandle {
   doogie_msgs::MazeCell getMatrixElement(doogie_msgs::MazeCellMultiArray matrix, int i, int j);
   void setMatrixElement(doogie_msgs::MazeCellMultiArray matrix, int i, int j, bool north_element,
                         bool south_element, bool east_element, bool west_element, bool visited);
+  LocalCell GlobalToLocal(doogie_msgs::DoogiePosition);
 
  private:
   doogie_msgs::MazeCellMultiArray mat;
