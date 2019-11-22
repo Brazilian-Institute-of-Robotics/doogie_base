12dadeb395ba35392c09fec52cdf4bc0b397caa8
Caio <caioaamaral@gmail.com>
2019-11-22 20:37:48 -0300

    Add action client member to MouseHandle class

--- doogie_navigation/include/doogie_navigation/mouse_handle.hpp
+++ doogie_navigation/include/doogie_navigation/mouse_handle.hpp
@@ -1,22 +1,29 @@
 #ifndef DOOGIE_NAVIGATION_MOUSE_HANDLE
 #define DOOGIE_NAVIGATION_MOUSE_HANDLE
 
+#include <actionlib/client/simple_action_client.h>
 #include "doogie_msgs/DoogiePosition.h"
+#include "doogie_msgs/DoogieMoveGoal.h"
+#include "doogie_msgs/DoogieMoveAction.h"
 
 namespace doogie_navigation{
 enum Direction{
-FWD, BACK, RIGHT, LEFT
+FWD=1u, BACK, RIGHT, LEFT
 };
 
 class MouseHandle{
 
 public:
+MouseHandle();
 doogie_msgs::DoogiePosition getPosition();
 void setPosition(doogie_msgs::DoogiePosition position);
-void move(Direction);
+void move(Direction direction);
 
 private:
 doogie_msgs::DoogiePosition position_;
+actionlib::SimpleActionClient<doogie_msgs::DoogieMoveAction> move_base_client_;
+doogie_msgs::DoogieMoveGoal goal_handle_;
+
 
 };
 }
--- doogie_navigation/src/mouse_handle.cpp
+++ doogie_navigation/src/mouse_handle.cpp
@@ -2,6 +2,12 @@
 
 namespace doogie_navigation{
 
+MouseHandle::MouseHandle() : move_base_client_("move_base", true){
+  ROS_INFO("Waiting for move_base server to start.");
+  move_base_client_.waitForServer();
+  ROS_INFO("move_base server started, sending goal.");
+}
+
 void MouseHandle::setPosition(doogie_msgs::DoogiePosition position){
   position_ = position;
 }
@@ -10,8 +16,9 @@ doogie_msgs::DoogiePosition MouseHandle::getPosition(){
   return position_;
 }
 
-void MouseHandle::move(Direction){
-
+void MouseHandle::move(Direction direction){
+  goal_handle_.direction = direction;
+  move_base_client_.sendGoal(goal_handle_);
 }
 
 }
