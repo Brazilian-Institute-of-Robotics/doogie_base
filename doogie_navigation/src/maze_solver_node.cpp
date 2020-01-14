0a766386923bb5117cbac1525888d652105988f7
Caio <caioaamaral@gmail.com>
2020-01-14 14:23:00 -0300

    Add maze_solver_node to doogie_navigation

--- doogie_navigation/src/maze_solver_node.cpp
+++ doogie_navigation/src/maze_solver_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <pluginlib/class_loader.h>
 #include "doogie_algorithms/solver_plugin_loader.hpp"
 
 int main (int argc, char** argv)
@@ -11,12 +12,13 @@ int main (int argc, char** argv)
   ros::init(argc, argv, "maze_solver");
   ros::NodeHandle nh;
   
-  doogie_algorithms::SolverPluginLoader plugin_loader(package, base_class);
+  doogie_algorithms::SolverPluginLoader plugin_loader("doogie_algorithms", "doogie_algorithms::BaseSolver");
 
   if (nh.getParam("/solver_plugin", plugin_param_name))
   {  
     solver = plugin_loader.getSolverInstance(plugin_param_name);
     solver->init();
+    solver.reset();
   }
   
   else{
