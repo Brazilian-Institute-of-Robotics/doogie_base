cc943738eff282ea1970123d7af0920183b043a4
Caio Amaral <caioaamaral@gmail.com>
2020-01-19 19:03:11 -0300

    Add loop function in maze_solver_node

--- doogie_navigation/src/maze_solver_node.cpp
+++ doogie_navigation/src/maze_solver_node.cpp
@@ -1,23 +1,28 @@
 #include <ros/ros.h>
 #include <pluginlib/class_loader.h>
 #include "doogie_algorithms/solver_plugin_loader.hpp"
+#define BASE_CLASS_PACKAGE "doogie_algorithms"
+#define BASE_CLASS "doogie_algorithms::BaseSolver"
+
+const std::string& package = "doogie_algorithms";
+const std::string& base_class = "doogie_algorithms::BaseSolver";
+
+void loop(const boost::shared_ptr<doogie_algorithms::BaseSolver>& solver);
 
 int main (int argc, char** argv)
 {
-  const std::string& package = "doogie_algorithms";
-  const std::string& base_class = "doogie_algorithms::BaseSolver";
   std::string plugin_param_name;
   boost::shared_ptr<doogie_algorithms::BaseSolver> solver;
   
   ros::init(argc, argv, "maze_solver");
-  ros::NodeHandle nh;
+  ros::NodeHandle ph("~");
   
-  doogie_algorithms::SolverPluginLoader plugin_loader("doogie_algorithms", "doogie_algorithms::BaseSolver");
+  doogie_algorithms::SolverPluginLoader plugin_loader(BASE_CLASS_PACKAGE, BASE_CLASS);
 
-  if (nh.getParam("/solver_plugin", plugin_param_name))
+  if (ph.getParam("/solver_plugin", plugin_param_name))
   {  
     solver = plugin_loader.getSolverInstance(plugin_param_name);
-    solver->init();
+    loop(solver);
     solver.reset();
   }
   
@@ -25,4 +30,12 @@ int main (int argc, char** argv)
     ROS_ERROR("Could not find /solver_plugin param, check if it's setted");
   }
     
+}
+
+void loop(const boost::shared_ptr<doogie_algorithms::BaseSolver>& solver){
+  while(ros::ok()){
+    solver->makePlan();
+    solver->move();
+    solver->sleep();
+  }
 }
