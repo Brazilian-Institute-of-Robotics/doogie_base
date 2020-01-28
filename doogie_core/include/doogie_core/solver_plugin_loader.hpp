442dd66534809b1aceaf526855964f8961a4290d
Caio Amaral <caioaamaral@gmail.com>
2020-01-28 12:30:07 -0300

    Update doogie_core - bring all typedef to the beginning of class definition - add configureSolverFromParams(void) - add get/set solver_name_ methods - restore doogiePositionCallback and mazeObstacleMatrixCallback methods - add (doogie_msgs::DoogieMoveGoal) _goal member to BaseSolver - change maze_matrix type to MazeMatrixPtr - add smart pointer initialization of MazeMatrixHandle member (MazeCellMultiArrayPtr maze_matrix) - add maze_matrix subscriber - add start_solver flag - add to base solver waitForStart() - silence move_base client inside mouse_handle

--- doogie_core/include/doogie_core/solver_plugin_loader.hpp
+++ doogie_core/include/doogie_core/solver_plugin_loader.hpp
@@ -5,14 +5,17 @@ namespace doogie_core{
 
 class SolverPluginLoader{
   public:
-    SolverPluginLoader(const std::string& package, const std::string& base_class) : 
-                                          solver_loader_(package, base_class){}
+    SolverPluginLoader(const std::string& package, const std::string& base_class);
+    void setSolverNameFromParams();
+    std::string getSolverName();
+    boost::shared_ptr<doogie_core::BaseSolver> getSolverInstance();                                          
     boost::shared_ptr<doogie_core::BaseSolver> getSolverInstance(const std::string& solver_param_name);
 
   private:
-    std::string solver_plugin_name_;
+    ros::NodeHandle ph_{"~"};
+    std::string solver_name_{"doogie_algorithms/RightHandSolverPlugin"};
     pluginlib::ClassLoader<doogie_core::BaseSolver> solver_loader_;
     boost::shared_ptr<doogie_core::BaseSolver> solver_;
 };
 
-};
+} //namespace doogie_core
