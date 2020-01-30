610d9e85f70bc654fa8620e45604720d8980dfe5
Caio Amaral <caioaamaral@gmail.com>
2020-01-29 21:27:46 -0300

    Update doogie_navigation - remove getparams method from maze_solver_node, all params are being received from SolverPluginLoader class - add initialize solver call before starting loop - add waitForStart inside plugin loader

--- doogie_navigation/src/maze_solver_node.cpp
+++ doogie_navigation/src/maze_solver_node.cpp
@@ -8,27 +8,17 @@ const std::string& BASE_CLASS = BASE_CLASS_PACKAGE+"::BaseSolver";
 
 void loop(const boost::shared_ptr<doogie_core::BaseSolver>& solver);
 
-int main (int argc, char** argv)
-{
-  std::string plugin_param_name;
-  boost::shared_ptr<doogie_core::BaseSolver> solver;
+int main (int argc, char** argv){
   
   ros::init(argc, argv, "maze_solver");
   ros::NodeHandle ph("~");
   
   doogie_core::SolverPluginLoader plugin_loader(BASE_CLASS_PACKAGE, BASE_CLASS);
+  boost::shared_ptr<doogie_core::BaseSolver> solver = plugin_loader.getSolverInstance();
 
-  if (ph.getParam("/solver_plugin", plugin_param_name))
-  {  
-    solver = plugin_loader.getSolverInstance(plugin_param_name);
-    loop(solver);
-    solver.reset();
-  }
-  
-  else{
-    ROS_ERROR("Could not find /solver_plugin param, check if it's setted");
-  }
-    
+  solver->waitForStart();
+  loop(solver);
+  solver.reset();
 }
 
 void loop(const boost::shared_ptr<doogie_core::BaseSolver>& solver){
