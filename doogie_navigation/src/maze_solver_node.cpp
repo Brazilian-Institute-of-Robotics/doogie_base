3c739a3b8db6a11eb650ab9b4eaca437f5960fe0
Caio Amaral <caioaamaral@gmail.com>
2021-03-01 22:11:38 +0000

    Changes solver_node since BaseSolver was moved to doogie_algorithms

--- doogie_navigation/src/maze_solver_node.cpp
+++ doogie_navigation/src/maze_solver_node.cpp
@@ -1,27 +1,28 @@
 #include <ros/ros.h>
 #include <pluginlib/class_loader.h>
-#include "doogie_core/solver_plugin_loader.hpp"
+#include "doogie_algorithms/solver_plugin_loader.hpp"
+#include "doogie_algorithms/base_solver.hpp"
 
 
-const std::string& BASE_CLASS_PACKAGE = "doogie_core";
+const std::string& BASE_CLASS_PACKAGE = "doogie_algorithms";
 const std::string& BASE_CLASS = BASE_CLASS_PACKAGE+"::BaseSolver";
 
-void loop(const boost::shared_ptr<doogie_core::BaseSolver>& solver);
+void loop(const boost::shared_ptr<doogie_algorithms::BaseSolver>& solver);
 
 int main (int argc, char** argv){
   
   ros::init(argc, argv, "maze_solver");
   ros::NodeHandle ph("~");
   
-  doogie_core::SolverPluginLoader plugin_loader(BASE_CLASS_PACKAGE, BASE_CLASS);
-  boost::shared_ptr<doogie_core::BaseSolver> solver = plugin_loader.getSolverInstance();
+  doogie_algorithms::SolverPluginLoader plugin_loader(BASE_CLASS_PACKAGE, BASE_CLASS);
+  boost::shared_ptr<doogie_algorithms::BaseSolver> solver = plugin_loader.getSolverInstance();
 
   solver->waitForStart();
   loop(solver);
   solver.reset();
 }
 
-void loop(const boost::shared_ptr<doogie_core::BaseSolver>& solver){
+void loop(const boost::shared_ptr<doogie_algorithms::BaseSolver>& solver){
   while(ros::ok()){
     solver->makePlan();
     solver->move();
